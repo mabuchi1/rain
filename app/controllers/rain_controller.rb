f7df4ce430fc87d615719bfcab3f9fd345e1d34e
mabuchi1 <msanhm6@gmail.com>
2019-09-25 18:41:40 +0900

    Update rain_controller.rb

--- app/controllers/rain_controller.rb
+++ app/controllers/rain_controller.rb
@@ -1,37 +1,40 @@
 class RainController < ApplicationController
-  require 'line/bot'
-
-  def callback
-    body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
+    require 'line/bot'  # gem 'line-bot-api'
+  
+    # callbackアクションのCSRFトークン認証を無効
+    protect_from_forgery :except => [:callback]
+  
+    def client
+      @client ||= Line::Bot::Client.new { |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      }
     end
-    events = client.parse_events_from(body)
-
-    events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-        end
+  
+    def callback
+      body = request.body.read
+  
+      signature = request.env['HTTP_X_LINE_SIGNATURE']
+      unless client.validate_signature(body, signature)
+        head :bad_request
       end
-      client.reply_message(event['replyToken'], message)
+  
+      events = client.parse_events_from(body)
+  
+      events.each { |event|
+        case event
+        when Line::Bot::Event::Message
+          case event.type
+          when Line::Bot::Event::MessageType::Text
+            message = {
+              type: 'text',
+              text: event.message['text']
+            }
+            client.reply_message(event['replyToken'], message)
+          end
+        end
+      }
+  
+      head :ok
     end
-    head :ok
   end
-
-private
-
-# LINE Developers登録完了後に作成される環境変数の認証
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
-end
